b6f0f1e8b34681920698d3e65807f3db66bebe6b
Brendan Moroso and Maya Frame <maya@>
2019-04-03 15:40:35 -0700

    Add first two specs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 // Business Logic
-var pigLatin = function(languageInput){
-  return true
+// var vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
+var ay = "ay"
+// var way = "way"
+
+function pigLatin(languageInput){
+  var array = languageInput.split("");
+  if (array.length === 1){
+    return (languageInput + ay)
+  }
+  else {
+    return "this is hard"
+  }
 }
 
+    
 // Basic User Interface
 $(document).ready(function(){
   $("form#pig-latin").submit(function(event){
---
+++ js/unused scripts.js
@@ -0,0 +1,14 @@
+// function translate(languageInput)
+var vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
+var ay = "ay"
+var way = "way"
+words = languageInput.split(" ")
+var words = words.map
+function lettersFunction(words){
+  var pigLatin = function(languageInput){
+
+
+var moreThan1 = [];
+words.forEach(function(word){
+  if(word.length >= 1){
+    moreThan1.push("ay")
