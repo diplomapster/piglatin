7d27bbe22a11366551e99baab136927543b74868
Brendan Moroso and Maya Frame <maya@>
2019-04-03 16:41:43 -0700

    add half of fourth spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 // Business Logic
-var vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
+// var vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
 var ay = "ay"
 var way = "way"
+
+function conWords(languageInput){
+  if (languageInput.charAt(0) !== ("a" || "e" || "i" || "o" || "u" || "y")){
+    // var firstLetter = languageInput.charAt(0)
+    // return (languageInput + firstLetter + ay)
+    return (languageInput.slice(1) + ay)
+  }
+  else {
+    return "This is still really hard"
+  }
+}
 
 function pigLatin(languageInput){
   if (languageInput.length === 1){
@@ -26,7 +37,7 @@ $(document).ready(function(){
   $("form#pig-latin").submit(function(event){
     event.preventDefault();
     var languageInput = $("input#language-input").val();
-    var languageOutput = vowelWords(languageInput);
+    var languageOutput = conWords(languageInput);
     
     $("#result").text(languageOutput);
   });
